d3811978b3c6d449faa20c879c9ebcd17e7ddeb3
Renan M. Batista <renanmourabatista@gmail.com>
2020-10-14 13:14:47 -0400

    [NEW] - integrado com backend

---
+++ src/utils/mapIcon.ts
@@ -0,0 +1,12 @@
+import mapMarkerImg from '../images/map-marker.svg';
+import L from 'leaflet';
+
+const mapIcon = L.icon({
+    iconUrl: mapMarkerImg,
+  
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+  })
+  
+  export default mapIcon;
